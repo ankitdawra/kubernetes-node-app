366bca3be91f4a9067600a49e6c1eb7c120dd51e
ankit.dawra <ankit.dawra@nagarro.com>
2023-06-18 16:45:05 +0530

    Final commit

--- index.js
+++ index.js
@@ -2,28 +2,57 @@ const express = require('express');
 const mysql = require('mysql');
 const app = express();
 const { MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DB } = process.env;
-const connection = mysql.createConnection({
-  host     : MYSQL_HOST,
-  user     : MYSQL_USER,
-  password : MYSQL_PASSWORD,
-  database : MYSQL_DB
-});
 const port = 5000;
+const errors = {
+  'DB_SERVER': 'Ughh! Database server not up properly. Please re-try! (Can use readme file for reference from step 1)',
+  'DB_DUMP': 'Ughh! Database not up properly. Please re-try! (Can use readme file for reference from step 2)',
+  'DB_TABLE': 'Ughh! Looks like database table not setup properly. Please re-try! (Can use readme file for reference step 2.5)'
+}
 
 app.use(express.static('public'));
 app.set("view engine", "ejs");
 
 app.get('/', (req, res) => {
-  const get_users_query = 'SELECT * from user_data;';
-  connection.query(get_users_query, function (err, result) {
-    if (err) {
-      console.log(err);
-      throw err;
-    }
-    res.render("index", {users: result});
+  const connection = mysql.createConnection({
+    host     : MYSQL_HOST,
+    user     : MYSQL_USER,
+    password : MYSQL_PASSWORD,
   });
+
+  connection.connect((err) => {
+    if (err) {
+      res.render("index", {users: [], error: errors['DB_SERVER']});
+      connection.end();
+    } else {
+      queryDb(connection, res);
+    }
+  })
 });
 
+function queryDb(connection, res) {
+  const connect_db = `USE ${MYSQL_DB};`;
+  connection.query(connect_db, err => {
+    if (err) {
+      res.render("index", {users: [], error: errors['DB_DUMP']});
+      connection.end();
+    } else {
+      queryTable(connection, res);
+    }
+  })
+}
+
+function queryTable(connection, res) {
+  const get_users_query = 'SELECT * from user_data;';
+  connection.query(get_users_query, (err, result) => {
+    if (err) {
+      res.render("index", {users: [], error: errors['DB_TABLE']});
+      connection.end();
+    } else {
+      res.render("index", {users: result, error: false});
+    }
+  });
+}
+
 app.listen(port, () => {
-    console.log(`Now listening on port ${port}`);
+  console.log(`Now listening on port ${port}`);
 });
